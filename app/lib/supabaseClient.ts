f73c40476c8548342f9b8e81c749b90b5f9b1971
Muntazir <muntazirmohammad07@gmail.com>
2025-11-23 04:06:07 +0500

    remove changepass

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,8 +1,7 @@
 // lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../supabasetypes";
 
-export const supabase = createClient<Database>(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
